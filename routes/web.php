3065f8a103196ae73b894117cd0d1e053a3b227c
jose2basoa <jose.de.barros0192@gmail.com>
2026-02-23 19:21:59 -0300

    commit

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,8 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\UserController;
 
+Route::redirect("/", "users");
+
 Route::resource('users', UserController::class);
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
